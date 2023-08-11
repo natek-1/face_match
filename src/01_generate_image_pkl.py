eb5883977299c6f51d2d561f833e40ddef745bc1
natek-1 <nategabrielk@icloud.com>
2023-08-11 15:17:04 -0400

    done creating pkl folder for images

--- src/01_generate_image_pkl.py
+++ src/01_generate_image_pkl.py
@@ -11,18 +11,39 @@ logging.basicConfig(filename=os.path.join(log_dir, 'runnig_log.log'),
                      level=logging.INFO, format=logging_str, filemode='a')
 
 def generate_data_pickfle_file(config_path: str, params_path):
+    # Reading information from out config 
     config = read_yaml(config_path)
     params = read_yaml(params_path)
 
+    #Gathering information about those config 
     artifacts = config['artifacts']
     artifacts_dir = artifacts['artifacts_dir']
     pickle_format_data_dir= artifacts['pickle_format_data_dir']
-    img_pick_file_name = artifacts['img_pickle_file_name']
+    img_pickle_file_name = artifacts['img_pickle_file_name']
     feature_extractor_dir = artifacts['feature_extraction_dir']
     extracted_features_name = artifacts['extracted_features_name']
 
+    ## Setting up folder where our images will be for easy access
     raw_local_dir_path = os.path.join(artifacts_dir, pickle_format_data_dir)
     create_directory(dirs=[raw_local_dir_path])
+
+
+    pickle_file = os.path.join(raw_local_dir_path, img_pickle_file_name)
+    data_path = params['base']['data_path']
+
+    actors = os.listdir(data_path)
+
+    # Getting the list of all the files with out data 
+    filenames = []
+    for actor in actors:
+        actor_path = os.path.join(data_path, actor)
+        for file in os.listdir(actor_path):
+            filenames.append(os.path.join(actor_path, file))
+    logging.info(f'Total number of actors are: {len(actors)}')
+    logging.info(f'Total number of images are: {len(filenames)}')
+
+    pickle.dump(filenames, open(pickle_file, 'wb'))
+
 
 
 if __name__ == '__main__':
